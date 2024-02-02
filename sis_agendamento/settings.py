dd36f180e79fdbf68f777a30a1a3597ee8c23997
edoardo colares <edoardo.colares@defensoria.ro.def.br>
2024-02-02 17:23:55 -0300

    refatorando

--- sis_agendamento/settings.py
+++ sis_agendamento/settings.py
@@ -24,7 +24,7 @@ dotenv.load_dotenv()
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = config('SECRET_KEY', default='foo')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
