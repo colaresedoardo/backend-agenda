236c186ed52e68beb2e0e19a0f2513bc5cb5830b
edoardo colares <edoardo.colares@defensoria.ro.def.br>
2024-02-02 16:17:37 -0300

    refatorando

--- sis_agendamento/settings.py
+++ sis_agendamento/settings.py
@@ -14,6 +14,7 @@ from pathlib import Path
 from datetime import timedelta
 import os
 import dotenv
+import django_on_heroku
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -155,7 +156,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR + '/static'
+# STATIC_ROOT = BASE_DIR + '/static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
@@ -169,3 +170,5 @@ else:
         "http://localhost:3000/",
         "http://127.0.0.1:3000/",
     ]
+
+django_on_heroku.settings(locals())
